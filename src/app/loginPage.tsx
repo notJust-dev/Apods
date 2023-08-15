aed15f45fa924af2e6e500bd05c59db6b3bb2493
Rohit Jain <rohit_@Rohits-MacBook-Air.local>
2023-08-15 08:14:39 +0530

    initial

--- src/app/loginPage.tsx
+++ src/app/loginPage.tsx
@@ -1,14 +1,14 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
 
-export class index extends Component {
+export class loginPage extends Component {
   render() {
     return (
       <View>
-        <Text>index</Text>
+        <Text>loginPage</Text>
       </View>
     )
   }
 }
 
-export default index
+export default loginPage
